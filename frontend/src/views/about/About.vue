7dc91abcf2c71791e8b8a16d403c00b6ec03e757
Seolhun <shun10114@gmail.com>
2018-10-24 04:18:34 +0900

    [f-about] add about component

--- frontend/src/views/about/About.vue
+++ frontend/src/views/about/About.vue
@@ -1,11 +1,49 @@
 <template>
-  <section>
-    <h2>About</h2>
+  <section class="container">
+    <h2>Teams</h2>
+    <div class="row">
+      <template fragment v-for="team in temas">
+        <TeamCard
+          class="col s4 team-detail"
+          :key="team.name"
+          :profile="team.profile"
+          :name='team.name'
+          :description='team.description'
+        />
+      </template>
+    </div>
   </section>
 </template>
 
 <script>
+import TeamCard from '@/components/about/TeamCard';
+
 export default {
-  components: {},
+  components: {
+    TeamCard,
+  },
+  data: () => {
+    return {
+      temas: [
+        {
+          profile: '@/assets/img/teams/seolhun.jpg',
+          name: 'Seolhun',
+          description: 'Hello',
+        },
+        {
+          profile: '@/assets/img/teams/seolhun.jpg',
+          name: 'Seolhun2',
+          description: 'Hello',
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+.team-detail {
+  margin: 10px;
+}
+</style>
+
