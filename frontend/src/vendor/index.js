6c5b0e2305f257132286bdbe1a31f7a774af2a4f
Seolhun <shun10114@gmail.com>
2018-10-24 04:19:15 +0900

    [f-vendor] remove unsafed import files

--- frontend/src/vendor/index.js
+++ frontend/src/vendor/index.js
@@ -1,13 +1,14 @@
+import 'vue-material/dist/vue-material.min.css'
 import '@/assets/scss/materialize.scss';
 import '@/assets/scss/main.css';
 
-import '@/assets/js/bin/materialize.min.js';
-// import '@/assets/js/carousel.js';
-// import '@/assets/js/sidenav.js';
-// import '@/assets/js/modal.js';
+// import '@/assets/js/bin/materialize.min.js';
+// // import '@/assets/js/carousel.js';
+// // import '@/assets/js/sidenav.js';
+// // import '@/assets/js/modal.js';
 
-import 'hammerjs';
-import $ from 'jquery';
+// import 'hammerjs';
+// import $ from 'jquery';
 
-global.$ = $;
-window.$ = $;
+// global.$ = $;
+// window.$ = $;
