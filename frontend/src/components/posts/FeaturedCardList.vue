f0cc6d455458d4c0486ed3b689f6ae1af4cad7f5
Seolhun <shun10114@gmail.com>
2018-10-24 04:18:14 +0900

    [f-posts] add posts component and controller to handle api

---
+++ frontend/src/components/posts/FeaturedCardList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="feature-card">
+    <template
+      fragment
+      v-for="post in posts"
+    >
+      <PostCard
+        class="post-card"
+        :key="post._id"
+        :post="post"
+      />
+    </template>
+  </div>
+</template>
+
+
+<script>
+import PostCard from '@/components/posts/PostCard';
+
+export default {
+  components: {
+    PostCard,
+  },
+  props: {
+    posts: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  created() {},
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.feature-card {
+  margin: 20px 0 10px;
+  padding: 10px;
+  background-color: #f3f3f3;
+}
+.post-detail {
+  flex: 1 1 auto;
+  flex-direction: column;
+}
+.post-image {
+  flex: 0 0 auto;
+  width: 160px;
+  height: 104px;
+}
+</style>
+
--- frontend/src/views/posts/PostList.vue
+++ frontend/src/views/posts/PostList.vue
@@ -1,11 +1,55 @@
 <template>
-  <section>
-    <h2>PostList</h2>
+  <section class="container">
+    <div class="row">
+      <FeaturedCardList
+        :posts="posts"
+      />
+    </div>
+    <div class="row">
+      <template fragment v-for="post in posts">
+        <PostCard
+          class="post-card"
+          :key="post._id"
+          :post="post"
+        />
+      </template>
+    </div>
   </section>
 </template>
 
 <script>
+import PostCard from '@/components/posts/PostCard';
+import FeaturedCardList from '@/components/posts/FeaturedCardList';
+
+import { getPosts } from '@/controllers/PostsControllers';
+
 export default {
-  components: {},
+  components: {
+    PostCard,
+    FeaturedCardList,
+  },
+  created() {
+    this.getPostsFromAPI();
+  },
+  data: () => {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getPostsFromAPI() {
+      getPosts().then((data) => {
+        this.posts = data;
+      });
+    },
+  },
 };
 </script>
+
+
+<style scoped>
+.post-card {
+  display: flex;
+}
+</style>
+
